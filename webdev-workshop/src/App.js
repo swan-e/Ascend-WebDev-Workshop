27fa649eaa3b173be7781924b45294bf00bf95a3
Patrick Badiang <patrickbadiang@Patricks-MacBook-Pro.local>
2025-11-06 17:16:29 -0500

    Added MaterialUI

--- webdev-workshop/src/App.js
+++ webdev-workshop/src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <Typography variant="h1">Hello World</Typography>
+      <Button variant="contained">Click Me</Button>
+    </Card>
   );
 }
 

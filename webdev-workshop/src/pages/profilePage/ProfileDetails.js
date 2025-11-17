87e911621a8a61bdfc6c1870493cbc6e51ba6032
Patrick Badiang <patrickbadiang@Patricks-MacBook-Pro.local>
2025-11-16 19:12:51 -0500

    Generalized profile details

--- webdev-workshop/src/pages/profilePage/ProfileDetails.js
+++ webdev-workshop/src/pages/profilePage/ProfileDetails.js
@@ -5,15 +5,15 @@ export default function ProfileDetails() {
         <Stack spacing={6}>
           <Box>
               <Typography sx={{fontSize: 100, lineHeight: 1, color: 'primary.main'}}>Aspiring</Typography>
-              <Typography sx={{fontSize: 100, lineHeight: 1, color: 'tertiary.main'}}>Software Engineer</Typography>
+              <Typography sx={{fontSize: 100, lineHeight: 1, color: 'tertiary.main'}}>--Enter position--</Typography>
           </Box>
           <Box>
               <Typography sx={{fontSize: 50, lineHeight: 1, fontWeight: 'bold',color: 'primary.main'}} >Education</Typography>
-              <Typography sx={{fontSize: 50, lineHeight: 1, color: 'primary.main'}}>Virginia Tech, Class of 2026</Typography>
+              <Typography sx={{fontSize: 50, lineHeight: 1, color: 'primary.main'}}>Virginia Tech, Class of --Enter Graduation Year--</Typography>
           </Box>
           <Box maxWidth={'83%'}>
               <Typography sx={{fontSize: 50, lineHeight: 1, color: 'primary.main'}}>What's my story?</Typography>
-              <Typography sx={{fontSize: 40, lineHeight: 1, color: 'secondary.main'}}>I started out in 2019 with Game Development, where I found a passion in creating products from the ground up teaching myself skills along the way. This passion has transferred over to Software Development where I’m excited to continuously learn new skills</Typography>
+              <Typography sx={{fontSize: 40, lineHeight: 1, color: 'secondary.main'}}>--Enter in your story--</Typography>
           </Box>
         </Stack>
     )
